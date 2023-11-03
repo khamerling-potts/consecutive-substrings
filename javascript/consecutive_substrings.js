df7a9c840921245789774e2ca643230130d8e43c
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-03 10:04:59 -0400

    done

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,19 +1,44 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  const result = [];
+  const stringArr = string.split("");
+  stringArr.forEach((char, index) => {
+    let substring = "";
+    stringArr.slice(index).forEach((subchar) => {
+      substring += subchar;
+      result.push(substring);
+    });
+  });
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
-  console.log("=>", consecutiveSubstrings('abc'));
+  console.log("=>", consecutiveSubstrings("abc"));
 
   console.log("");
 
   console.log("Expecting: ['a']");
-  console.log("=>", consecutiveSubstrings('a'));
+  console.log("=>", consecutiveSubstrings("a"));
+
+  console.log("expecting [w, we, wer, were, e, er, ere, r, re, e]");
+  console.log(consecutiveSubstrings("were"));
+
+  console.log("expecting []");
+  console.log(consecutiveSubstrings(""));
 }
 
 module.exports = consecutiveSubstrings;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+initialize result array as []
+turn string into array
+for each char in string, include index
+  const substring = '';
+  for each subchar in string.slice(index)
+    substring += subchar
+    push substring into result array
+return result
+*/
